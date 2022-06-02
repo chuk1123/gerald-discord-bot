c2537f452ed54f5b2d46c8ceabb2f23bd892368c
chuk1123 <chuk1123@icloud.com>
2022-06-01 19:22:13 -0700

    fix bugs

--- main.py
+++ main.py
@@ -8,36 +8,33 @@ import asyncio
 import pytz
 from helper import get_day, update_day
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+def get_penguin_role(message):
+    penguin_role = discord.utils.get(
+    message.guild.roles, id=981439948152528986)
+    return penguin_role
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@tasks.loop(hours=24)
-async def daily_task():
-    message_channel = client.get_channel(969586867513217064)
-    week_day_num = date.today().weekday()
-    if week_day_num in [0, 1, 3]:
-        await message_channel.send("!newday")
-        await message_channel.send("!tasks")
-    if str(date.today()) == '2022-06-03':
-        await asyncio.sleep(60)
-        await message_channel.send("I SELF DESTRUCT!!!")
-
-@daily_task.before_loop
-async def before():
-    for _ in range(60*60*24):  # loop the whole day
-        utc_now = pytz.utc.localize(datetime.utcnow())
-        pst_now = utc_now.astimezone(pytz.timezone("America/Los_Angeles"))
-
-        if pst_now.hour == 8:  # 24 hour format
-            print('It is 8. Time to start the loop')
-            return
-        await asyncio.sleep(1)# wait a second before looping again. You can make it more 
-
 @client.event
 async def on_message(message):
-    pass
+    if message.content.startswith('!hi'):
+        await message.channel.send('Hi, I am Gerald the sensational penguin!')
+    if message.content.startswith('!penguins') or message.content.startswith('!penguin'):
+        await message.channel.send('Penguins:')
+        penguins = []
+        penguin_mem = get_penguin_role(message).members
+        for penguin in penguin_mem:
+            if penguin.nick == None:
+                penguins.append(str(penguin.name))
+            else:
+                penguins.append(str(penguin.nick))
+        await message.channel.send("\n".join(member for member in penguins))
 
 client.run('OTY4OTA3MzA4ODExODE2OTgx.YmlrPw.Z-EtIYiqORR2alkyemKe85f9nMY')
